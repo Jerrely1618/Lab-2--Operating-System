c105cd7901b7231749f45e28e845bd2030611f6a
Jerrely1618 <victorv2718@gmail.com>
2022-09-11 23:50:52 -0400

    Lab_2 v1.0

---
+++ Lab 2/Problem1.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+int main (int argc, char* argv[])
+{
+    char* filepath = argv[1];
+    int returnval;
+    // Check file existence
+    returnval = access (filepath, F_OK);
+    
+    if(returnval == 0){
+        printf ("%s exists\n", filepath);
+    }else{
+        if(errno == ENOENT){
+            printf ("%s does not exist\n", filepath);
+        }
+        else if(errno == EACCES){
+            printf ("%s is not accessible\n", filepath);
+        }
+    }
+    returnval = access (filepath, R_OK);
+    if(returnval != 0){
+            printf ("%s cannot be read\n", filepath);
+    }
+    returnval = access (filepath, W_OK);
+    if(errno != 0){
+        printf ("%s cannot write\n", filepath);
+    }
+    return 0;
+}
