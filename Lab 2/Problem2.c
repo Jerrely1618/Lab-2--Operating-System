3338435fd11fdcd52f00689f8be6912639b040ef
Jerrely1618 <victorv2718@gmail.com>
2022-09-16 18:51:25 -0400

    Lab_2 v1.3

--- Lab 2/Problem2.c
+++ Lab 2/Problem2.c
@@ -7,24 +7,19 @@
 #include <errno.h>
 int main(int argc, char* argv[]){
     char* filepath = argv[1];
-    int ret,returnval,fd;
-    char buf[1024];
-    
-    returnval = access(filepath, F_OK);
-    if(returnval == 0){
-        fd = open(filepath,O_RDWR);
-        ret = read(fd, buf, sizeof(buf)-1);
-        buf[ret] = 0x00;
-        printf("%s\n", buf);
-        close(fd);
+    int fd,ret;
+    fd = open(filepath,O_RDONLY);
+    if(fd == -1){
+        perror("Error");
     }
     else{
-        if(errno == ENOENT){
-            printf ("%s does not exist\n", filepath);
+        char c;
+        ret = read(fd, &c, 1);
+        while(ret != 0){
+            printf("%c",c);
+            ret = read(fd, &c, 1);
         }
-        else if(errno == EACCES){
-            printf ("%s is not accessible\n", filepath);
-        }
+        close(fd);
     }
         
 //https://www.ibm.com/docs/en/i/7.2?topic=ssw_ibm_i_72/apis/write.htm
--- Lab 2/Problem4.c
+++ Lab 2/Problem4.c
@@ -5,11 +5,10 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-#include <stdlib.h>
 int main(int argc, char* argv[]){
     char* SourceFile = argv[1];
     char* DestinationFile = argv[2];
-    int sf,df,ret,writ,off;
+    int sf,df,off,ret,writ;
     
     sf = open(SourceFile,O_RDONLY);
     df = open(DestinationFile,O_WRONLY);
@@ -21,9 +20,13 @@ int main(int argc, char* argv[]){
         char c;
         ret = read(sf, &c, 1);
         while(ret != 0){
+            char * buf;
             writ = write(df,&c,1);
             ret = read(sf, &c, 1);
         }
+        char* buf = "XYZ";
+        writ = write(df,buf,3);
+
         close(sf);
         close(df);
     }
